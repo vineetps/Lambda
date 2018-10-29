89018a7ef97d475309fbe9cd2f6c3b411b735934
Vineet Pal Singh <37205839+vineetps@users.noreply.github.com>
2018-10-29 10:51:25 +0530

    updated with iam certs

--- DeleteExpiredCert.py
+++ DeleteExpiredCert.py
@@ -1,29 +1,50 @@
-import boto3
-import json
-
-client = boto3.client('acm')
+import boto3, datetime, json
 
 def lambda_handler(event, context):
     length = []
     
+    client = boto3.client('acm')
     response = client.list_certificates()
-    
+    print 'ACM Certificate : \n'
     for certArn in response['CertificateSummaryList']:
-        CertificateArn = certArn['CertificateArn']
-        CertificateDomain = certArn['DomainName']
+        acmCertArn = certArn['CertificateArn']
+        acmCertDomain = certArn['DomainName']
         
         DesCert = client.describe_certificate(
-            CertificateArn=CertificateArn
+            CertificateArn=acmCertArn
         )
-     
+        
         if DesCert['Certificate']['Status'] == 'EXPIRED' and DesCert['Certificate']['InUseBy'] == []:
-            length.append(CertificateDomain)
+            length.append(acmCertDomain)
             
-            // below API is for delete certificate
-            // comment below 3 lines to not delete the ACM Certs
-            delCert = client.delete_certificate(
-                CertificateArn=CertificateArn
-            )
+            # below API is for delete certificate
 
-    print 'Cert count : '+str(len(length)),'\nCert arn : '+str(length)
-    return 'Cert count : '+str(len(length)),length
+            # uncomment below 3 lines to delete the ACM Certs
+            # delCert = client.delete_certificate(
+            #     CertificateArn=acmCertArn
+            # )
+            print 'Expired : (',acmCertDomain,')'
+        else:
+            print 'Active : (',acmCertDomain,')'
+            
+    client = boto3.client('iam')
+
+    response = client.list_server_certificates()
+    print '\n\nIAM Certificate : \n'
+    
+    for iamCert in response['ServerCertificateMetadataList']:
+        iamCertName = iamCert['ServerCertificateName']
+        iamCertExpiration = iamCert['Expiration']
+
+        if str(iamCertExpiration) < str(datetime.datetime.now()):
+            # below API is for delete certificate
+
+
+            # Uncomment below 3 lines to enable deletion of expired IAM Certificates
+            # response = client.delete_server_certificate(
+            #     ServerCertificateName= iamCertName
+            # )
+            
+            print 'Expired : (',iamCertName,')'
+        else:
+            print 'Active : (',iamCertName,')'
